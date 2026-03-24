61f1b4d2aaf6094f76a9eab6f6578700b095e5fd
kanywst <niwatakuma@icloud.com>
2026-03-24 23:15:27 +0900

    f

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -3,7 +3,6 @@
  * Enables bulk imports
  */
 
-export { default as Footer } from './Footer';
-export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
 export { default as MainLayout } from './MainLayout';
 export { default as MuiProvider } from './MuiProvider';
